6cb3bd245524a6f4e83bb90aaba11c79110ddc1d
Dinuka Rathnayake <it22140616@my.sliit.lk>
2025-07-29 09:15:03 +0530

    Implement Main Class
    - Created java project
    - Created mathematic operations in Main class

---
+++ Java Calculator/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // variable declaration & initiation
+        double number1 = 0;
+        double number2 = 0;
+        char operator;
+        double result = 0;
+
+        // user inputs
+        Scanner scannerObject = new Scanner(System.in);
+
+        System.out.print("Enter first number  : ");
+        number1 = scannerObject.nextDouble();
+
+        System.out.print("Enter operator      : ");
+        operator = scannerObject.next().charAt(0);
+
+        System.out.print("Enter second number : ");
+        number2 = scannerObject.nextDouble();
+
+        // calculation
+        switch (operator) {
+            case '+' -> result = number1 + number2;
+            case '-' -> result = number1 - number2;
+            case '*' -> result = number1 * number2;
+            case '/' -> result = number1 / number2;
+            default -> {
+                System.out.println("Invalid operator");
+            }
+        }
+
+        // display output
+        System.out.println("Answer is         : " + result);
+
+        scannerObject.close();
+    }
+}
